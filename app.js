667b94177e562df7233a887bb7464d354d3aaf2a
Rishav Agarwal <rishavcoder@gmail.com>
2020-04-24 02:09:33 +0530

    design

--- app.js
+++ app.js
@@ -186,19 +186,6 @@ function handleRedirect(req, res) {
  * Everytime the server recieves a request, this function is called
  */
 function serveListener(req, res) {
-	const headers = {
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Headers": "X-Requested-With",
-		"Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-		"Access-Control-Max-Age": 2592000, // 30 days
-		/** add other headers as per requirement */
-	};
-
-	if (req.method === "OPTIONS") {
-		res.writeHead(204, headers);
-		return res.end();
-	}
-
 	const url = req.url;
 
 	// Home page
